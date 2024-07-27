b2a60115e22d12e5805104119e5f2646a946e3ad
jimmyraj18 <jimmysdm0711@gmail.com>
2024-07-27 18:57:20 +0600

    Create Account.py

---
+++ Account.py
@@ -0,0 +1,22 @@
+from abc import ABC
+
+class Account(ABC):
+
+    #Name
+    def getname(self):
+        pass
+
+    def setname(self,name):
+        pass
+
+    def getaccountnumber(self):
+        pass
+
+    def setaccountnumber(self,accountnumber):
+        pass
+
+    def getbalance(self):
+        pass
+
+    def setbalance(self,balance):
+        pass
